f5c41ed2e523a0819384eb28c14da2f92568eb25
MuffinKing-jpeg <58091875+MuffinKing-jpeg@users.noreply.github.com>
2021-02-10 14:55:06 +0200

    Initial commit

---
+++ functions/functions/host2check.js
@@ -0,0 +1,21 @@
+module.exports  = [
+  {
+    name: 'Основной шлюз',
+    port: 88,
+    host: 'play.logicworld.ru',
+    icon: 'fas fa-ethernet'
+  },
+  {
+    name: 'Главный сервер',
+    port: 80,
+    host: 'play.logicworld.ru',
+    icon: 'fas fa-server'
+  },
+  {
+    name: 'Второй сервер',
+    port: 80,
+    host: 's2.logicworld.ru',
+    icon: 'fas fa-server'
+  },
+    
+]
