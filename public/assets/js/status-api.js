52d1ac452307f67f0ab7ba11477924cf59c78094
MuffinKing-jpeg <andriydobrus@gmail.com>
2022-01-21 22:37:33 +0200

    Implemented sumarry status

--- public/assets/js/status-api.js
+++ public/assets/js/status-api.js
@@ -68,26 +68,80 @@ async function refresh_status_i() {
         );
 }
 
+async function qr_gen(content, bg, color, id) {
+    var qrcode = new QRCode({
+        padding: 3,
+        background: bg,
+        color: color,
+        content: content,
+        container: "svg-viewbox", //Responsive use
+        join: true
+
+    });
+    document.getElementById(id).innerHTML = qrcode.svg();
+    return 0;
+}
+
 async function refresh_status_g() {
     const container = document.getElementById('status-container-g');
     setTimeout(document.getElementById('status-container-g').style.opacity = 0, 10)
     setTimeout(document.getElementById('spiner_container-g').style.opacity = 1, 800)
-    fetch("https://logicworld.ru/monAJAX/cache/cache.json")
+    fetch("https://logicworld.ru/monAJAX/cache/cache.json?nocache=" + new Date().getTime())
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            let time = new Date()
             container.innerHTML = "";
-        })
-        .catch(
-            (err) => {
-                container.innerHTML = "";
-                container.appendChild(document.createElement('div')).id = 'error-g';
-                document.getElementById('error-g').innerHTML = 'Something went wrong: <div class=error_text>' + err + '</div>';
-                document.getElementById('spiner_container-g').style.opacity = 0;
-                setTimeout(document.getElementById('status-container-g').style.opacity = 1, 400)
-
+            container.appendChild(document.createElement("div")).id = "status-summary-g"
+            for (let i = 0; i <= 1; i++) {
+                document.getElementById("status-summary-g").appendChild(document.createElement("div")).className = "summary-container";
             }
-        );
+            const summ_cont = document.getElementsByClassName("summary-container");
+            summ_cont[0].appendChild(document.createElement("div")).id = "qr"
+            summ_cont[1].appendChild(document.createElement("div")).id = "summary-status-container-g"
+            summ_cont[1].style = "width:100%;";
+            document.getElementById("summary-status-container-g").appendChild(document.createElement("table")).id = "summary-status-g";
+            document.getElementById("summary-status-container-g").appendChild(document.createElement("div")).id = "summary-status-progressbar-g";
+
+            for (let i = 0; i < 3; i++) {
+                document.getElementById("summary-status-g").appendChild(document.createElement("tr")).className = "summary-status-tr-g";
+                for (let j = 0; j < 4; j++) {
+                    document.getElementsByClassName("summary-status-tr-g")[i].appendChild(document.createElement("td")).className = "summary-status-td-g";
+                }
+            }
+            const summ_td = document.getElementsByClassName("summary-status-td-g");
+            summ_td[0].innerHTML = "Online:";
+            summ_td[1].innerHTML = data.online + "/" + data.slots
+            summ_td[2].innerHTML = `${time.toLocaleString("ru-RU")}`
+            summ_td[3].innerHTML = "<i class=\"fas fa-user-friends\"></i>"
+            summ_td[4].innerHTML = "Рекорд дня"
+            summ_td[5].innerHTML = data.recordday
+            summ_td[6].innerHTML = data.timerecday
+            summ_td[7].innerHTML = "<i class=\"fas fa-trophy\"></i>"
+            summ_td[8].innerHTML = "Рекорд"
+            summ_td[9].innerHTML = data.record
+            summ_td[10].innerHTML = data.timerec
+            summ_td[11].innerHTML = "<i class=\"fas fa-trophy\"></i>"
+
+
+            qr_gen("https://logicworld.ru/", "none", "var(--text-secondary)", "qr");
+        })
+        .then(() => {
+            document.getElementById('spiner_container-g').style.opacity = 0;
+            setTimeout(document.getElementById('status-container-g').style.opacity = 1, 400)
+
+        })
+        // .catch(
+        //     (err) => {
+        //         console.error(err);
+        //         container.innerHTML = "";
+        //         container.appendChild(document.createElement('div')).id = 'error-g';
+        //         document.getElementById('error-g').innerHTML = 'Something went wrong: <div class=error_text>' + err + '</div>';
+        //         document.getElementById('spiner_container-g').style.opacity = 0;
+        //         setTimeout(document.getElementById('status-container-g').style.opacity = 1, 400)
+
+        //     }
+        // );
 
 }
 
