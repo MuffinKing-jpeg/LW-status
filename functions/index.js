d50f2bc9c7e8c826cfb9f898ce2389b0930458e8
MuffinKing-jpeg <andriydobrus@gmail.com>
2022-03-16 20:47:36 +0200

    Loading configs and serverlist from DB

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,70 @@
 const functions = require("firebase-functions");
 const isPortReachable = require('is-port-reachable');
 const admin = require('firebase-admin');
+const { log } = require("firebase-functions/logger");
 
 
 admin.initializeApp();
 const db = admin.firestore()
 
-// const host2check = require("./host2check");
+let config;
+async function getConfig() {
+  await db.collection("config").doc("properties").get().then((doc) => {
+    if (doc.exists) {
+      config = doc.data();
+      console.log(config);
+    } else {
+      config = {
+        timeout: 1000
+      }
+    }
+  })
+}
+getConfig();
+
+async function ping(data) {
+  if (await isPortReachable(data.port,
+    {
+      host: data.host,
+      timeout: config.timeout
+    }
+  ) === true) {
+    return 'Online'
+  } else {
+    return 'Offline'
+  }
+}
 
 async function state() {
   var status
   await db.collection("config").doc("servers").get()
-    .then((doc) => {
+    .then(async (doc) => {
       if (doc.exists) {
-        host2check == doc.data()
+        let host2check = doc.data()
+        host2check = host2check.servers;
         status = new Array(host2check.length);
-        // for (let i = 0; i < status.length; i++) {
-        //   status[i] = {
-        //     name: host2check[i].name,
-        //     icon: host2check[i].icon
-        //   }
-        //   if (await isPortReachable(host2check[i].port,
-        //     {
-        //       host: host2check[i].host,
-        //       timeout: 250
-        //     }
-        //   ) === true) {
-        //     status[i].avalible = 'Online'
-        //   } else {
-        //     status[i].avalible = 'Offline'
-        //   }
-        // }
-        console.log(doc.data());
+        for (let i = 0; i < status.length; i++) {
+          status[i] = {
+            name: host2check[i].name,
+            icon: host2check[i].icon,
+            avalible: await ping({ port: host2check[i].port, host: host2check[i].host })
+          }
+        }
       } else {
         // doc.data() will be undefined in this case
-
-
+        status = {
+          name: "Database",
+          icon: "fa-solid fa-database",
+          avalible: "Offline"
+        }
       }
     })
     .catch((error) => {
-      console.log("Error getting documents: ", error);
-
+      status = {
+        name: error,
+        icon: "fa-solid fa-bug",
+        avalible: "Offline"
+      }
     });
 
 
