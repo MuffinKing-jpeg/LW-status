ac673c46597f1b6a54cb2f35b7c6e1aa4b71f7ed
MuffinKing-jpeg <andriydobrus@gmail.com>
2022-03-16 17:37:08 +0200

    Implemented loading configs from DB

--- functions/index.js
+++ functions/index.js
@@ -1,28 +1,49 @@
 const functions = require("firebase-functions");
 const isPortReachable = require('is-port-reachable');
+const admin = require('firebase-admin');
 
-const host2check = require("./host2check");
+
+admin.initializeApp();
+const db = admin.firestore()
+
+// const host2check = require("./host2check");
 
 async function state() {
-  var status = new Array(host2check.length);
-  for (let i = 0; i < status.length; i++) {
-    status[i] = {
-      name: host2check[i].name,
-      icon: host2check[i].icon,
-      host: host2check[i].host,
-      port: host2check[i].port
-    }
-    if (await isPortReachable(host2check[i].port,
-      {
-        host: host2check[i].host,
-        timeout: 2500
+  var status
+  await db.collection("config").doc("servers").get()
+    .then((doc) => {
+      if (doc.exists) {
+        host2check == doc.data()
+        status = new Array(host2check.length);
+        // for (let i = 0; i < status.length; i++) {
+        //   status[i] = {
+        //     name: host2check[i].name,
+        //     icon: host2check[i].icon
+        //   }
+        //   if (await isPortReachable(host2check[i].port,
+        //     {
+        //       host: host2check[i].host,
+        //       timeout: 250
+        //     }
+        //   ) === true) {
+        //     status[i].avalible = 'Online'
+        //   } else {
+        //     status[i].avalible = 'Offline'
+        //   }
+        // }
+        console.log(doc.data());
+      } else {
+        // doc.data() will be undefined in this case
+
+
       }
-    ) === true) {
-      status[i].avalible = 'Online'
-    } else {
-      status[i].avalible = 'Offline'
-    }
-  }
+    })
+    .catch((error) => {
+      console.log("Error getting documents: ", error);
+
+    });
+
+
   console.log(status);
   return status
 }
